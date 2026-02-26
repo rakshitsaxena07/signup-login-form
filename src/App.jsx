59bb2de8fa2b7975cb6f6249e63d77046145ce5d
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-26 12:36:19 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/login';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
-    <div className='font-bold text-2xl text-center'>
-      helo
-    </div>
+  <BrowserRouter>
+  
+     <Routes>
+       <Route path="/" element={<Navigate to="/login" />} />
+       <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound/>} />
+     </Routes>
+    </BrowserRouter>
   )
 }
