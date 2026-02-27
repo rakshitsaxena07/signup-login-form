e54bdc787bfbb666bc5db788c82a5b0e537b716f
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-02-27 10:24:12 +0530

    feat: add protected dashboard route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Signup from './pages/Signup';
-import Login from './pages/login';
-import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/login";
+import NotFound from "./pages/NotFound";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-  <BrowserRouter>
-  
-     <Routes>
-       <Route path="/" element={<Navigate to="/login" />} />
-       <Route path="/signup" element={<Signup />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound/>} />
-     </Routes>
+        <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
